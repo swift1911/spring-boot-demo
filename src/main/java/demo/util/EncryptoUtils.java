3efb0266662033f1169c0b4cca6aed4c3cd77972
swift du（杜雅林） <swift1911@163.com>
2015-10-31 09:45:31 +0800

    update

--- src/main/java/demo/util/EncryptoUtils.java
+++ src/main/java/demo/util/EncryptoUtils.java
@@ -1,7 +1,11 @@
 package demo.util;
 
+import java.io.Serializable;
+
 /**
  * Created by swift on 15/9/12.
  */
-public class EncryptoUtils {
+public class EncryptoUtils implements Serializable{
+
+    
 }
